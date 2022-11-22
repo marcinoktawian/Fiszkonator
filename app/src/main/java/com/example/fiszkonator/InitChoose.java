d15243a22dbf3ec999cbc915fd62297eb081647f
Szatkowski Marcin <marcin.szatkowski@nokia.com>
2022-11-22 15:22:00 +0100

    Create choose view

---
+++ app/src/main/java/com/example/fiszkonator/InitChoose.java
@@ -0,0 +1,14 @@
+package com.example.fiszkonator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitChoose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init_choose);
+    }
+}
